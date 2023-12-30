ed79f5c15ff89afe8fcd13965f1c26137ac2907a
Alvaro <alvaromagu2002@gmail.com>
2023-12-30 17:41:03 +0100

    move drawing code to canvas hook

---
+++ src/hooks/use-gravity-canvas.ts
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useOnWindowResize } from './use-window-resize'
+import { type Planet } from '../models/planet'
+
+export function useGravityCanvas ({
+  canvasRef
+}: {
+  canvasRef: React.RefObject<HTMLCanvasElement>
+}) {
+  const [ctx, setCtx] = useState(canvasRef.current?.getContext('2d'))
+
+  useOnWindowResize(({ width, height }) => {
+    if (canvasRef.current != null) {
+      canvasRef.current.width = width
+      canvasRef.current.height = height
+    }
+  })
+
+  useEffect(() => {
+    setCtx(canvasRef.current?.getContext('2d'))
+  }, [canvasRef.current?.getContext('2d')])
+
+  const draw = useCallback(({
+    planets
+  }: {
+    planets: Planet[]
+  }) => {
+    if (ctx == null) return
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    planets.forEach(planet => {
+      const radius = planet.radius
+      const diameter = radius * 2
+      const x = planet.position.x - radius
+      const y = planet.position.y - radius
+      ctx.beginPath()
+      ctx.roundRect(x, y, diameter, diameter, radius)
+      ctx.fillStyle = planet.texture
+      ctx.fill()
+      ctx.closePath()
+      if (planet.trail.length > 1) {
+        ctx.beginPath()
+        ctx.moveTo(planet.trail[0].x, planet.trail[0].y)
+        for (let i = 1; i < planet.trail.length; i++) {
+          ctx.lineTo(planet.trail[i].x, planet.trail[i].y)
+        }
+        ctx.strokeStyle = planet.texture
+        ctx.stroke()
+        ctx.closePath()
+      }
+    })
+  }, [ctx])
+
+  return {
+    draw
+  }
+}
--- src/hooks/use-gravity.ts
+++ src/hooks/use-gravity.ts
@@ -1,6 +1,5 @@
 import { useRequestFrame } from './use-request-frame'
-import { useOnWindowResize } from './use-window-resize'
-import { useCanvas } from './use-canvas'
+import { useGravityCanvas } from './use-gravity-canvas'
 import { usePlanets } from './use-planets'
 import { useCallback } from 'react'
 
@@ -9,42 +8,13 @@ export function useGravity ({
 }: {
   canvasRef: React.RefObject<HTMLCanvasElement>
 }) {
-  const { ctx } = useCanvas({ canvasRef })
+  const { draw } = useGravityCanvas({ canvasRef })
   const { planetsRef, update: updatePlanets } = usePlanets()
 
-  useOnWindowResize(({ width, height }) => {
-    if (canvasRef.current != null) {
-      canvasRef.current.width = width
-      canvasRef.current.height = height
-    }
-  })
-
   const onFrame = useCallback(() => {
-    if (ctx == null) return
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     updatePlanets()
-    planetsRef.current.forEach(planet => {
-      const radius = planet.radius
-      const diameter = radius * 2
-      const x = planet.position.x - radius
-      const y = planet.position.y - radius
-      ctx.beginPath()
-      ctx.roundRect(x, y, diameter, diameter, radius)
-      ctx.fillStyle = planet.texture
-      ctx.fill()
-      ctx.closePath()
-      if (planet.trail.length > 1) {
-        ctx.beginPath()
-        ctx.moveTo(planet.trail[0].x, planet.trail[0].y)
-        for (let i = 1; i < planet.trail.length; i++) {
-          ctx.lineTo(planet.trail[i].x, planet.trail[i].y)
-        }
-        ctx.strokeStyle = planet.texture
-        ctx.stroke()
-        ctx.closePath()
-      }
-    })
-  }, [ctx])
+    draw({ planets: planetsRef.current })
+  }, [draw])
 
   useRequestFrame(onFrame)
 }
